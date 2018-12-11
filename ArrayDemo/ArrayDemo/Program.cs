d651b3f921f41e596596a4161191de99e119c37c
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-12-11 11:54:10 -0600

    update

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[8];
+            int i;
+          
+            for (i = 0; i < 8; i++)
+            {
+                Write("Enter a Number - " + i + " <<");
+                arr[i] = Convert.ToInt32(ReadLine());
+            }
+
+            int option;
+
+            WriteLine("Choose option 1 for viewing the list in order first to last postion." +
+                "Choose option 2 for view the order last to first. Choose option 3 to select a position.");
+            option = Convert.ToInt32(ReadLine());
+
+            if (option == 1)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    WriteLine(arr[j]);
+                }
+            }
+            else if (option == 2)
+            {
+                for (int b = 0; b < arr.Length; ++b )
+                {
+                    Array.Reverse(arr);
+                    WriteLine(arr[b]);
+                }
+            }
+
+        }
+    }
+}
---
+++ RentPrice/RentPrice/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RentPrice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFloors = new System.Windows.Forms.TextBox();
+            this.txtRooms = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFloors
+            // 
+            this.txtFloors.Location = new System.Drawing.Point(146, 34);
+            this.txtFloors.Name = "txtFloors";
+            this.txtFloors.Size = new System.Drawing.Size(100, 20);
+            this.txtFloors.TabIndex = 0;
+            // 
+            // txtRooms
+            // 
+            this.txtRooms.Location = new System.Drawing.Point(146, 79);
+            this.txtRooms.Name = "txtRooms";
+            this.txtRooms.Size = new System.Drawing.Size(100, 20);
+            this.txtRooms.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Room Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number of Rooms";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(146, 138);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(111, 198);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(165, 46);
+            this.lblResult.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(158, 280);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 366);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRooms);
+            this.Controls.Add(this.txtFloors);
+            this.Name = "Form1";
+            this.Text = "RentPrice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFloors;
+        private System.Windows.Forms.TextBox txtRooms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
