7294a4d7473da177b731f6d42c907b2343799787
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-12-12 12:49:17 -0600

    update

--- CheckZips/CheckZips/Program.cs
+++ CheckZips/CheckZips/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
 namespace CheckZips
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] zipCode = { 11111, 12685, 77777, 12345, 77677, 22222, 10023, 54321, 20012, 97654};
+            int userZipe;
+
+            WriteLine("Enter a 5 digit Zip Code ");
+            userZipe = Convert.ToInt32(ReadLine());
+
+            for (int i = 0; i < zipCode.Length; i++)
+            {
+                if(zipCode[i] == userZipe)
+                {
+                    WriteLine("We can deliver");
+                }
+                else
+                    WriteLine("We can't");
+
+            }
+            
+
+
+
+
+
+
+
         }
     }
 }
